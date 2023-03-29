7a1947c46b1d536cd929c308f0a1177fc70d7d9a
chthonic3 <chthonic3@gmail.com>
2023-03-29 08:09:36 +0800

    新增註解

--- app.py
+++ app.py
@@ -8,47 +8,53 @@ import time
 app = Flask(__name__)
 
 #
+
+
 @app.route('/')
 def main():
-    #展示廠的資料
+    # 展示廠的資料
     sql = 'select id,machineTyp1,machineNo1,loc1,place1 from mac_loc where enable1="1" ORDER BY machineTyp1 ASC, machineNo1 ASC'
     datas = db.query_data(sql)
     return render_template('index.html', datas=datas)
 
+
 @app.route('/<machineTyp>/<machineNo>')
-def index(machineTyp,machineNo):
+def index(machineTyp, machineNo):
+    # 將系統本日日期丟入date變數
     date = datetime.date.today()
-    #搜尋資料庫是否有此機型或機號，limit就是只要有就停止搜尋
-    sql = 'select machineTyp,machineNo from user where machineTyp="'+ machineTyp +'"' +'and machineNo="'+machineNo+'" limit 1'
-    #將搜尋到的資料存放在datas下備用
+    # 搜尋資料庫是否有此機型或機號，limit就是只要有就停止搜尋，代表是有項目的
+    sql = 'select machineTyp,machineNo from user where machineTyp="' + \
+        machineTyp + '"' + 'and machineNo="'+machineNo+'" limit 1'
+    # 將搜尋到的資料存放在datas下備用，給index.html提取資料用
     datas = db.query_data(sql)
-    #查詢有資料就放在exit_datas為1，沒有就為0
-    exit_datas=len(datas)
-    if exit_datas==1:#如果有資料
-        #用來html顯示順序備用
-        sql1 = 'select * from user where machineTyp="'+ machineTyp +'"' +'and machineNo="'+machineNo+'" ORDER BY date DESC'
+    # 查詢有資料就放在exit_datas為1，沒有就為0
+    exit_datas = len(datas)
+    if exit_datas == 1:  # 如果有資料
+        # 用來html顯示順序備用
+        sql1 = 'select * from user where machineTyp="' + machineTyp + \
+            '"' + 'and machineNo="'+machineNo+'" ORDER BY date DESC'
         datas1 = db.query_data(sql1)
 
-        #抓取此機台最後一筆日期，與今日比較用
-        sql2 = 'SELECT date FROM user where machineTyp="'+ machineTyp +'"' +'and machineNo="'+machineNo+'" ORDER BY date DESC LIMIT 1'
-        datas2 = db.query_data(sql2)     
-        #   
+        # 抓取此機台最後一筆日期，與今日比較用
+        sql2 = 'SELECT date FROM user where machineTyp="' + machineTyp + \
+            '"' + 'and machineNo="'+machineNo+'" ORDER BY date DESC LIMIT 1'
+        datas2 = db.query_data(sql2)
+        #
         for item in datas2:
             for key in item:
-                data_date=item[key]
+                data_date = item[key]
 
-        if data_date !=date:
-            update_date='今日無更新'
+        if data_date != date:
+            update_date = '今日無更新'
         else:
-            update_date='已更新'
-        
-        return render_template('show_add_user.html', machineTyp=machineTyp,machineNo=machineNo,date=date,datas1=datas1,update_date=update_date)
+            update_date = '已更新'
+
+        return render_template('show_add_user.html', machineTyp=machineTyp, machineNo=machineNo, date=date, datas1=datas1, update_date=update_date)
     else:
-        return render_template('show_add_user.html', machineTyp=machineTyp,machineNo=machineNo,date=date)
+        return render_template('show_add_user.html', machineTyp=machineTyp, machineNo=machineNo, date=date)
 
 
-
-@app.route("/do_add_user",methods=['POST'])
+@app.route("/do_add_user", methods=['POST'])
 def do_add_user():
     machineTyp = request.form.get("machineTyp")
     machineNo = request.form.get("machineNo")
@@ -56,32 +62,33 @@ def do_add_user():
     user = request.form.get("user")
     date = request.form.get("date")
     # print(user)
-    if machineTyp and machineNo and backlog and user and date !="":
+    if machineTyp and machineNo and backlog and user and date != "":
         sql = f"""
             insert into user (machineTyp , machineNo , backlog , user , date)
             values('{machineTyp}' , '{machineNo}' , '{backlog}' , '{user}' , '{date}')
         """
         print(sql)
         db.insert_or_update_data(sql)
-        return render_template("success.html",machineTyp=machineTyp,machineNo=machineNo)
+        return render_template("success.html", machineTyp=machineTyp, machineNo=machineNo)
     else:
         return render_template("err.html")
 
-@app.route("/do_update_user/<machineTyp>/<machineNo>/<user_id>/<date>",methods=['POST','GET'])
-def do_update_user(machineTyp,machineNo,user_id,date):
+
+@app.route("/do_update_user/<machineTyp>/<machineNo>/<user_id>/<date>", methods=['POST', 'GET'])
+def do_update_user(machineTyp, machineNo, user_id, date):
     machineTyp = machineTyp
     machineNo = machineNo
-    date = request.form.get("date")
-    print(date)
+
     sql = f"""
             UPDATE user SET enddate = '{date}' WHERE id = '{user_id}'
-        """ 
+        """
     db.insert_or_update_data(sql)
-    return render_template("success_update.html",machineTyp=machineTyp,machineNo=machineNo)
+    return render_template("success_update.html", machineTyp=machineTyp, machineNo=machineNo)
 
-if __name__=="__main__":
-    app.debug=True
+
+if __name__ == "__main__":
+    app.debug = True
     app.run(host="0.0.0.0")
-    
+
     # #先將電腦的IP4改為IP，然後在下方host改為此電腦的ip，就可以連線
     # app.run(debug=True,host='192.168.185.16',port=5000)
